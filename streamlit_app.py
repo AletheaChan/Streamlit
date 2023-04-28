583293e2cbfc96deeab5d31c604a27f5ba02ce8f
AletheaChan <116297612+AletheaChan@users.noreply.github.com>
2023-04-29 01:11:58 +0800

    Update streamlit_app.py
    Interactive widget (multi select)

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Interactive widget (Multi-select), A pick list to pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Table display
 streamlit.dataframe(my_fruit_list)
